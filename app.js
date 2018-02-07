e5e1ec380e4b36cf5176bf8c02096bd0a3e221ae
rafanog <rafanog2155@gmail.com>
2018-02-07 15:53:31 -0400

    added ignored files and modified code

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-console.log('Just a test!');
+//testNodeJS
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const xhr = new XMLHttpRequest();
 
+var url
+
+ping();
+
+function ping(){
+	if(!process.argv[2]){
+		console.log('missing url parameter, to execute the app: node app *URL*');
+	}else{
+		xhr.open('GET', process.argv[2], false);
+		xhr.send(null);
+
+		if(!(xhr.status == 200)){
+			console.log('error: ' + xhr.status);
+		}else{
+			console.log('ok!');
+		}
+	}
+}
